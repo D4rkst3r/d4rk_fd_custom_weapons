1edec1d95e5a111a6c0d561288e7bce520351892
D4rkst3r <az3198284@gmail.com>
2026-02-07 23:06:04 +0100

    Add fire extinguisher weapon types with effects and properties
    - Introduced four types of fire extinguishers: Water, Foam, CO2, and Powder.
    - Each extinguisher has unique visual effects and sounds defined in weaponfx.meta.
    - Updated weapons.meta to include detailed properties for each extinguisher type, including model references, damage types, and firing mechanics.
    - Configured aiming, accuracy, and damage modifiers specific to each extinguisher.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,13 +10,13 @@ files {
 }
 
 -- Alle Meta-Dateien registrieren
-data_file 'WEAPONINFO_FILE' 'meta/fire_weapons.meta'
-data_file 'WEAPON_ANIMATIONS_FILE' 'meta/fire_weaponanimations.meta'
-data_file 'WEAPON_METADATA_FILE' 'meta/fire_weaponarchetypes.meta'
-data_file 'WEAPONFX_FILE' 'meta/fire_weaponfx.meta'
-data_file 'LOADOUTS_FILE' 'meta/fire_loadouts.meta'
+data_file 'WEAPONINFO_FILE' 'meta/weapons.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'meta/weaponanimations.meta'
+data_file 'WEAPON_METADATA_FILE' 'meta/weaponarchetypes.meta'
+data_file 'WEAPONFX_FILE' 'meta/weaponfx.meta'
+data_file 'LOADOUTS_FILE' 'meta/loadouts.meta'
 data_file 'TEXTFILE_METAFILE' 'meta/dlctext.meta'
-data_file 'PED_PERSONALITY_FILE' 'meta/fire_pedpersonality.meta'
+data_file 'PED_PERSONALITY_FILE' 'meta/pedpersonality.meta'
 
 client_scripts {
     'cl_weaponNames.lua',
